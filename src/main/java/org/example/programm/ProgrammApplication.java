bcddb65612a79d210b81c5b1ad00d10e4a8c059b
Annushkkka <anyuta260703@gmail.com>
2025-04-25 13:56:37 +0300

    Исправлены комментарии для JavaDoc

--- src/main/java/org/example/programm/ProgrammApplication.java
+++ src/main/java/org/example/programm/ProgrammApplication.java
@@ -3,11 +3,17 @@ package org.example.programm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* Класс, отвечающий за запуск нашего веб-приложения
+* */
 @SpringBootApplication
 public class ProgrammApplication {
 
+	/**
+	 * Главный метод запуска веб-приложения
+	 * @param args аргументы для запуска
+	 */
 	public static void main(String[] args) {
-
 		SpringApplication.run(ProgrammApplication.class, args);
 	}
 
